481ab2399ac7c2965eea6786eecd81c29723a6e2
Ruzhentsev Alexandr <webcode@pgallery.ru>
2018-01-16 10:28:47 +0300

    modify

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -29,6 +29,20 @@ if [ -f /var/www/html/config/nginx/nginx-vhost-ssl.conf ]; then
     cp /var/www/html/config/nginx/nginx-vhost-ssl.conf /etc/nginx/conf.d/default-ssl.conf
 else
 
+    if [ -f /etc/nginx/ssl/server.pem ] || [ -f /etc/nginx/ssl/server.key ]; then
+	# Проверяем, смонтирован ли внешний SSL сертификат
+
+	sed -i "s/\/etc\/ssl\/certs\/ssl-cert-snakeoil.pem/\/etc\/nginx\/ssl\/server.pem/g" /etc/nginx/conf.d/default-ssl.conf
+	sed -i "s/\/etc\/ssl\/private\/ssl-cert-snakeoil.key/\/etc\/nginx\/ssl\/server.key/g" /etc/nginx/conf.d/default-ssl.conf
+
+    elif [ -f /etc/letsencrypt/live/${SSL_DOMAIN}/fullchain.pem ] || [ -f /etc/letsencrypt/live/${SSL_DOMAIN}/privkey.pem ]; then
+	# Проверяем, существует ли Let's Encrypt сертификат
+
+	sed -i "s/\/etc\/ssl\/certs\/ssl-cert-snakeoil.pem/\/etc\/letsencrypt\/live\/${SSL_DOMAIN}\/fullchain.pem/g" /etc/nginx/conf.d/default-ssl.conf
+	sed -i "s/\/etc\/ssl\/private\/ssl-cert-snakeoil.key/\/etc\/letsencrypt\/live\/${SSL_DOMAIN}\/privkey.pem/g" /etc/nginx/conf.d/default-ssl.conf
+
+    fi
+
     sed -i \
         -e "s/FPM_HOST/${FPM_HOST}/g" \
         -e "s/FPM_PORT/${FPM_PORT}/g" \
